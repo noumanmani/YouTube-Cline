fa660379685c31ae482ab54b5b33a2c763e18ce7
noumanmani <168138758+noumanmani@users.noreply.github.com>
2024-07-14 00:14:53 +0500

    sceand page is complete

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var menu = document.querySelector('.menu')
 var sidebar = document.querySelector('.side-bar')
+var container = document.querySelector('.container')
+
 menu.onclick=function(){
-    sidebar.classList.toggle("small-sidebar")
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("larg-container")
 }
